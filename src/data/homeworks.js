051706c1f405e84e747e143c7780cd285522ed60
Andrii Stavskyi <an.stawski@outlook.com>
2025-09-22 01:14:22 +0300

    added "features" to homework page

--- src/data/homeworks.js
+++ src/data/homeworks.js
@@ -1,39 +1,42 @@
 export const homeworks = [
     {
-    slug: "homework-1",
-    title: "Homework 1",
-    desc: "Setup Local Env",
-    github: "https://github.com/stkossman/lab-1",
-    vercel: "https://lab-1-lilac.vercel.app",
-    details: {
-      goals: "Learn how to setup a local environment for React development.",
-      timeSpent: "Less than 5 minutes",
-      grade: "2 + 1 extra point",
+        slug: "homework-1",
+        title: "Homework 1",
+        desc: "Setup Local Env",
+        github: "https://github.com/stkossman/lab-1",
+        vercel: "https://lab-1-lilac.vercel.app",
+        features: [],
+        details: {
+            goals: "Learn how to setup a local environment for React development.",
+            timeSpent: "Less than 5 minutes",
+            grade: "2 + 1 extra point",
+        },
     },
-  },
     {
-    slug: "homework-2",
-    title: "Homework 2",
-    desc: "React Component. Props and State",
-    github: "https://github.com/stkossman/lab-2",
-    vercel: "https://lab-2-woad.vercel.app",
-    details: {
-      goals: "Learn how to create a React component with props and state.",
-      timeSpent:
-        "Less than 2 hours (since I used TypeScript for the first time)",
-      grade: "2 + 4 extra points",
+        slug: "homework-2",
+        title: "Homework 2",
+        desc: "React Component. Props and State",
+        github: "https://github.com/stkossman/lab-2",
+        vercel: "https://lab-2-woad.vercel.app",
+        features: ["TypeScript", "TailwindCSS"],
+        details: {
+            goals: "Learn how to create a React component with props and state.",
+            timeSpent:
+                "Less than 2 hours (since I used TypeScript for the first time)",
+            grade: "2 + 4 extra points",
+        },
     },
-  },
     {
         slug: "homework-3",
         title: "Homework 3",
         desc: "Component Tree. Hooks",
         github: "https://github.com/stkossman/lab-3",
         vercel: "https://lab-3-tau.vercel.app",
+        features: ["TypeScript", "TailwindCSS"],
         details: {
-            goals: "Learn how to build a composable React application by properly colocating state in the TodoList and individual TodoItem components",
-            timeSpent:
-                "Less than 2 hours",
+            goals:
+                "Learn how to build a composable React application by properly colocating state in the TodoList and individual TodoItem components",
+            timeSpent: "Less than 2 hours",
             grade: "to be determined",
         },
     },
